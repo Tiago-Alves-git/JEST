0b7ae86bf7ffcea37b34df344a6b46ea5e435add
Tiago Alves <tiagoalvesfernandes81@gmail.com>
2022-11-11 12:16:17 -0300

    :white_check_mark: Adiciona Req 06 e 07
    Vqv

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Verifica a Função Pokemon', () => {
+  test('Verifica a exibição das propriedades do Pokemon', () => {
+    renderWithRouter(<App />);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toHaveTextContent('Pikachu');
+
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType).toHaveTextContent('Electric');
+
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonWeight).toHaveTextContent('Average weight: 6.0 kg');
+
+    const pokemonImg = screen.getByAltText('Pikachu sprite');
+    expect(pokemonImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    /* expect(pokemonImg).toHaveAttribute('alt', 'Pikachu sprite'); */
+  });
+
+  test('Testa o Link Mais Detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(moreDetails);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  test('Todos Pokemons Favoritos tem estrela', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => history.push('/pokemons/25'));
+
+    const favChecked = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favChecked);
+
+    const favoritesPokemons = screen.getAllByRole('img');
+    expect(favoritesPokemons[1]).toHaveAttribute('src', '/star-icon.svg');
+    expect(favoritesPokemons[1]).toHaveAttribute('alt', 'Pikachu is marked as favorite');
+  });
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste do componente PokemonDetails', () => {
+  it('Testa se as informações detalhadas são mostradas na tela', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(details);
+    screen.getByRole('heading', { name: /pikachu details/i });
+    expect(details).not.toBeInTheDocument();
+
+    screen.getByRole('heading', { name: /summary/i });
+
+    screen.getByText(/this intelligent pokémon roasts hard berries with electricity to make them tender enough to eat\./i);
+  });
+  it('Testa se existe na página uma seção com os mapas contendo a localização do pokémon exibido', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(details);
+
+    screen.getByRole('heading', { name: /game locations of pikachu/i });
+    screen.getByText(/kanto viridian forest/i);
+    screen.getByText(/kanto power plant/i);
+
+    const imgs = screen.getAllByRole('img', {
+      name: /pikachu location/i,
+    });
+    expect(imgs[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(imgs[1]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  });
+  it('Testa se é possível favoritar um pokémon atráves da página de detalhes', () => {
+    renderWithRouter(<App />);
+    const details = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(details);
+    const checkbox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkbox);
+    expect(checkbox.checked).toBeTruthy();
+    userEvent.click(checkbox);
+    expect(checkbox.checked).toBeFalsy();
+  });
+});
